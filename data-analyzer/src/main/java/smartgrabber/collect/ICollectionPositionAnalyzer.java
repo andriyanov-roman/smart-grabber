ee3ef0f772d4bd6c2e1d6d78e3de2b87a2b809d3
lisunovdv <lisunovdv@gmail.com>
2015-10-24 14:55:10 +0300

    ending iFace

--- data-analyzer/src/main/java/smartgrabber/collect/ICollectionPositionAnalyzer.java
+++ data-analyzer/src/main/java/smartgrabber/collect/ICollectionPositionAnalyzer.java
@@ -1,9 +1,12 @@
 package smartgrabber.collect;
 
-import smartgrabber.collect.ICollectionCommonAnalyzer;
+import java.util.Collection;
 
 /**
  * Created by randriyanov on 24.10.15.
  */
 public interface ICollectionPositionAnalyzer extends ICollectionCommonAnalyzer {
+    Number checkTimeStartInserting (Collection collection);
+    Number checkTimeMiddleInserting(Collection collection);
+    Number checkTimeEndInserting(Collection collection);
 }
