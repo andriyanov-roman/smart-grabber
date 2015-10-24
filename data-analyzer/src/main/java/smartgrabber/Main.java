701219817832ebc9e06d3787abceee4b120a0a14
randriyanov <randriyanov@mail.ru>
2015-10-24 12:58:12 +0300

    new commit

--- data-analyzer/src/main/java/smartgrabber/Main.java
+++ data-analyzer/src/main/java/smartgrabber/Main.java
@@ -1,8 +1,18 @@
 package smartgrabber;
 
 
+import smartgrabber.collect.examples.CarMoveImpl;
+import smartgrabber.collect.examples.IMove;
+import smartgrabber.collect.examples.PlaneMoveImpl;
+import smartgrabber.collect.SimpleTest;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!!!!");
+        SimpleTest test = new SimpleTest();
+        IMove car = new CarMoveImpl();
+        test.getMove(car);
+        IMove plane = new PlaneMoveImpl();
+        test.getMove(plane);
     }
 }
