6abd97ad3c4a7eeb8d26f63fc592e0e8d884417f
randriyanov <randriyanov@mail.ru>
2015-10-24 14:40:29 +0300

    added interfaces logic

---
+++ data-analyzer/src/main/java/smartgrabber/collect/ICollectionPositionAnalyzer.java
@@ -0,0 +1,9 @@
+package smartgrabber.collect;
+
+import smartgrabber.collect.ICollectionCommonAnalyzer;
+
+/**
+ * Created by randriyanov on 24.10.15.
+ */
+public interface ICollectionPositionAnalyzer extends ICollectionCommonAnalyzer {
+}
---
+++ data-analyzer/src/main/java/smartgrabber/collect/impl/CollectionAnalyzerImpl.java
@@ -0,0 +1,12 @@
+package smartgrabber.collect.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by randriyanov on 24.10.15.
+ */
+public class CollectionAnalyzerImpl {
+}
