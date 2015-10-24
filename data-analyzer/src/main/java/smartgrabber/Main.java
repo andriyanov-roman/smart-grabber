e90632b77f91d8858cecac86d55ff6fb62a03ae3
randriyanov <randriyanov@mail.ru>
2015-10-24 11:55:29 +0300

    added analyzer module

--- data-analyzer/src/main/java/smartgrabber/Main.java
+++ data-analyzer/src/main/java/smartgrabber/Main.java
@@ -4,4 +4,7 @@ package smartgrabber;
  * Created by randriyanov on 24.10.15.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Alisa");
+    }
 }
