45bb395fac5106078b2087bb7ade0f3b5d825e6b
randriyanov <randriyanov@mail.ru>
2015-10-25 10:35:35 +0200

    moved to java package

--- data-analyzer/src/main/java/smartgrabber/collect2/IFindCollectionTime.java
+++ data-analyzer/src/main/java/smartgrabber/collect2/IFindCollectionTime.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 /**
  * Created by mit_OK! on 24.10.2015.
  */
-public interface IFindCollectionTime<T extends Collection> {
+public interface IFindCollectionTime<T extends Collection> extends IFindCollectionMemory {
    Long showTimeDeleting(T t);
    Long showTimeBeginInserting(T t);
    Long showTimeMiddleInserting(T t);
