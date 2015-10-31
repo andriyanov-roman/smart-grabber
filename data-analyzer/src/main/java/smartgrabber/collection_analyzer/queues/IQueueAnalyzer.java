a377a7787a1caaa44ce985a376818660ff7706b9
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2015-10-31 20:27:46 +0200

    QueueAnalyzer in progress

---
+++ data-analyzer/src/main/java/smartgrabber/collection_analyzer/queues/IQueueAnalyzer.java
@@ -0,0 +1,19 @@
+package smartgrabber.collection_analyzer.queues;
+
+
+import smartgrabber.collection_analyzer.IMemoryAnalyzer;
+
+import java.util.Collection;
+
+
+/**
+ * by Aliso4ka1313 on 31.10.2015.
+ */
+public interface IQueueAnalyzer <T extends Collection> extends IMemoryAnalyzer <T>{
+    @Override
+    Long findMemory(T t);
+
+
+    @Override
+    Long findTime(T t);
+}
---
+++ data-analyzer/src/test/java/smartgrabber/collection_analyzer/TestQueueAnalyzer.java
@@ -0,0 +1,27 @@
+package smartgrabber.collection_analyzer;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import smartgrabber.collection_analyzer.queues.IQueueAnalyzer;
+import smartgrabber.collection_analyzer.queues.QueueImpl;
+/**
+ * by Aliso4ka1313 on 31.10.2015.
+ */
+public class TestQueueAnalyzer {
+    @Test
+    public void queueTest(){
+        IQueueAnalyzer<Queue> analyzer = new QueueImpl();
+        Deque<String> deque = new ArrayDeque<>();
+        deque.add("aaaa");
+        deque.add("bbb");
+        deque.add("ccc");
+        deque.add("dd");
+        deque.add("e");
+
+        System.out.println(analyzer.findMemory(deque) + " bytes");
+
+
+}
+}
