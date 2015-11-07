a2b3bfcbf9fb8dfef0eafdafd44f4230f93938d2
lisunovdv <lisunovdv@gmail.com>
2015-11-08 00:45:34 +0200

    queues refactor

--- data-analyzer/src/main/java/smartgrabber/collection_analyzer/queues/Executable.java
+++ data-analyzer/src/main/java/smartgrabber/collection_analyzer/queues/Executable.java
@@ -7,5 +7,5 @@ import java.util.Queue;
  * Created by Администратор on 07.11.2015.
  */
 public interface Executable{
-    void execute (Queue queue);
+    void execute ();
 }
--- data-analyzer/src/main/java/smartgrabber/collection_analyzer/queues/ITimer.java
+++ data-analyzer/src/main/java/smartgrabber/collection_analyzer/queues/ITimer.java
@@ -3,6 +3,6 @@ package smartgrabber.collection_analyzer.queues;
 /**
  * Created by mit_OK! on 07.11.2015.
  */
-public interface ITimer {
-    Long retrieveSpendingTime();
+public interface ITimer<T extends Executable> {
+    Long retrieveSpendingTime(T t);
 }
