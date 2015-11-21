fd2ed4aa3028ffe37ac74128d578dc8ecafcf461
randriyanov <randriyanov@mail.ru>
2015-11-21 11:51:22 +0200

    temporary classes added

---
+++ company/src/main/java/repo/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package repo;
+
+
+import entity.Employee;
+
+public class EmployeeRepository implements CRUDRepository<Employee> {
+
+    public void save(Employee employee) {
+
+    }
+
+    public Employee read(Long id) {
+        return null;
+    }
+
+    public void delete(Employee employee) {
+
+    }
+
+    public Employee update(Employee employee) {
+        return null;
+    }
+}
