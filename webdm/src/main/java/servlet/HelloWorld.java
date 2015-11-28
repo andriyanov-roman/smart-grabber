9859b29a4269e9a47c1f0d6db6e2b4e6c54b3854
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2015-11-28 13:21:02 +0200

    tomcat test

---
+++ webdm/src/main/java/servlet/HelloWorld.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Администратор on 28.11.2015.
+ */
+public class HelloWorld extends HttpServlet {
+    private String message;
+    public void init() throws ServletException {
+       message ="Hello Alisa";
+    }
+    public void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>"+message+"</h1>");
+    }
+    public void destroy(){
+
+    }
+}
