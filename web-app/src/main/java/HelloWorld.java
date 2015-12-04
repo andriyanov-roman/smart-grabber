c92ad8f7e51b555ac548d89d4ec03fc5a216c52b
faust <lisunovdv@gmail.com>
2015-12-04 23:23:16 +0200

    Merge Web war Module with Daemon Brunch

--- web-app/src/main/java/HelloWorld.java
+++ web-app/src/main/java/HelloWorld.java
@@ -1,8 +1,20 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by faust on 28.11.15.
  */
 public class HelloWorld extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+
+    }
 }
