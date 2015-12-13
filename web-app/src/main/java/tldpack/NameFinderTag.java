137e5728d92cbe10879bf7314a5a3a0828c03dfa
faust <lisunovdv@gmail.com>
2015-12-13 12:11:31 +0200

    pack added

---
+++ web-app/src/main/java/tldpack/NameFinderTag.java
@@ -0,0 +1,17 @@
+package tldpack;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * Created by faust on 13.12.15.
+ */
+public class NameFinderTag extends TagSupport {
+
+    public NameFinderTag() {
+    }
+
+
+    public void retrieveName () {
+
+    }
+}
